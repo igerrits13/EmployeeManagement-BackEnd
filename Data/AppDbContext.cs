673bd39e0615c5c85a8e5a0831aaccb517bbf582
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2025-06-03 17:33:31 -0600

    Add database context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,13 +1,17 @@
+using EmployeeManagement.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeManagement.Data
 {
   public class AppDbContext : DbContext
   {
+    // Create a set to store data
+    public DbSet<Employee> Employees { get; set; }
+
     // ctor + tab
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-      
+
     }
   }
 }
